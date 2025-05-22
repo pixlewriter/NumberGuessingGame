5a98728569a43ddf538726319f1d84f97498c1fe
pixlewriter <ryan@kufeldt.com>
2025-05-22 06:53:33 -0400

    Python Number Guessing Game. v1.0

--- pythonMain.py
+++ pythonMain.py
@@ -1,14 +1,14 @@
-from random import * # type: ignore
+from random import *
 
 
 def runPlayer():
     randomNumber = randint(1,100)
-    print(randomNumber)
-    i = 5
+    maxGuesses = 5
+    i = maxGuesses
     while i > 0:
         pInput = input(f"You have {i} guesses left. Pick a number, 1-100: ")
         if int(pInput) == randomNumber:
-            print(f"You have picked the correct number! You've won! It took you {6-i} guesses")
+            print(f"You have picked the correct number! You've won! It took you {(maxGuesses+1)-i} guesses")
             break
         elif int(pInput) > randomNumber:
             print("lower")
@@ -18,10 +18,29 @@ def runPlayer():
         i -= 1
     
     if i == 0:
-        print("You lost")
+        print(f"You lost. The number was {randomNumber}")
 
 def runComputer():
-    pass
+    guessNumber = 50
+    highOrLow = {"h":1, "l": -1}
+    guesses = 5
+    numbers = [25, (12,13), (6,7), (3,4)]
+    gameWon = False
+    print(f"ok pick a number! \n I have {guesses} guesses. After each guess, tell me higher, lower, or correct")
+    for i in range(guesses):
+        answer = input(f"Guess {i+1}: {guessNumber}. (h/l/c)").lower()
+        if answer == "c": 
+            print(f"Yay, I won! it took me {i+1} guesses!")
+            gameWon = True
+            break
+        elif (answer == "h" or answer == "l") and i < 4:
+            if type(numbers[i]) == tuple:
+                numbers[i] = randint(numbers[i][0], numbers[i][1])
+            guessNumber+= numbers[i]*highOrLow[answer]
+    if gameWon == False:
+        print("Oh no! I lost!")
+
+
 
 def Main():
     playAgain = "y"
